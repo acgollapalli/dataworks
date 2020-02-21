97d541ad739ef6cc3dfad0d37f06ab8a864cf6c9
Akshay C. Gollapalli <acgollapalli@jnasquare.com>
2020-02-20 21:49:24 -0600

    Getting DB credentials from config.edn

--- src/dataworks/db/app_db.clj
+++ src/dataworks/db/app_db.clj
@@ -2,10 +2,17 @@
   (:require
    [monger.core :as mg]
    [monger.credentials :as mcr]
-   [mount.core :refer [defstate]]))
+   [mount.core :refer [defstate]]
+   [clojure.java.io :as io]
+   [clojure.edn :as edn]))
+
+(defn db-uri []
+  (-> "config.edn"
+      slurp
+      edn/read-string
+      :app-db-uri))
 
 (defstate app-db
   ;; TODO ADD CREDENTIALS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11111111111111111
   :start
-  (mg/get-db (mg/connect) "Dataworks_AppData"))
-
+   (:db (mg/connect-via-uri (db-uri))))
