7ee1fed7bb2cec252659bf5b3125b4f4c37e925e
Akshay C. Gollapalli <acgollapalli@jnasquare.com>
2020-03-21 00:33:00 -0500

    Added ->? macro, to aid in validation.

--- src/dataworks/core.clj
+++ src/dataworks/core.clj
@@ -10,19 +10,32 @@
    [yada.yada :refer [listener resource as-resource]])
   (:gen-class))
 
+(defmacro ->? [x & forms]
+  "A utility function for validation of data and transactions"
+  (loop [x x
+         forms forms]
+    (if-not forms
+      x
+      (let [next-form (first forms)
+            status? `(fn ~'[{:keys [status] :as params}]
+                       (if (= ~'status :failure)
+                         ~'params
+                         (-> ~'params ~next-form)))]
+        (recur (list status? x) (next forms))))))
+
 (def routes
-    ["/"
-     [["app/"
-       {"collector" c/collectors
-        "collector/" c/collector
-        "transactor" t/transactors
-        ["transactor/" :id] t/transactor
-        "internal" i/internals
-        ["internal/" :id] i/internal
-        "register" auth/register
-        "login" auth/login}]
-      ["user/" c/user]
-      [true (as-resource nil)]]])
+  ["/"
+   [["app/"
+     {"collector" c/collectors
+      "collector/" c/collector
+      "transactor" t/transactors
+      ["transactor/" :id] t/transactor
+      "internal" i/internals
+      ["internal/" :id] i/internal
+      "register" auth/register
+      "login" auth/login}]
+    ["user/" c/user]
+    [true (as-resource nil)]]])
 
 (def port
   (-> "config.edn"
