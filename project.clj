945c493ae47d019e100448d89b6b56231a9e6a68
Akshay C. Gollapalli <acgollapalli@jnasquare.com>
2020-08-31 16:22:00 -0500

    sync'd deps and lein

--- project.clj
+++ project.clj
@@ -13,13 +13,14 @@
                  [mount "0.1.16"]
                  [tick "0.4.25-alpha"]
                  [buddy/buddy-hashers "1.4.0"]
-                 [juxt/crux-core "20.05-1.8.4-alpha"]
-                 [juxt/crux-kafka "20.05-1.8.4-alpha"]
-                 [juxt/crux-rocksdb "20.05-1.8.4-alpha"]]
+                 [juxt/crux-core "20.08-1.10.1-alpha"]
+                 [juxt/crux-kafka "20.08-1.10.1-alpha"]
+                 [juxt/crux-rocksdb "20.08-1.10.1-alpha"]
+                 [camel-snake-kebab "0.4.1"]]
   :main ^:skip-aot dataworks.core
   :target-path "target/%s"
   :plugins  [[lein-cljfmt "0.6.7"]]
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[com.clojure-goes-fast/clj-memory-meter "0.1.2"]
-                                  [juxt/crux-kafka-embedded "20.05-1.8.4-alpha"]]
+                                  [juxt/crux-kafka-embedded "20.08-1.10.1-alpha"]]
                    :jvm-opts ["-Djdk.attach.allowAttachSelf"] }})
